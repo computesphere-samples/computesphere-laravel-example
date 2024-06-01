6ec26d2d5060e93bf6abc401c4559b950b81df3c
Gagandeep Singh <gagansday@gmail.com>
2024-06-02 05:07:14 +0530

    contact list

--- app/Livewire/Contact.php
+++ app/Livewire/Contact.php
@@ -6,6 +6,23 @@ use Livewire\Component;
 
 class Contact extends Component
 {
+    public $contacts = [];
+
+    public $name = '';
+
+    public $email = '';
+
+    public function save()
+    {
+        $this->contacts[] = $this->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email:rfc,dns',
+        ]);
+
+        $this->name = '';
+        $this->email = '';
+    }
+
     public function render()
     {
         return view('livewire.contact');
--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -4,4 +4,38 @@
     </h3>
     <p class="description text">This tool allows you to manage contacts using the form below.</p>
 
+    <form wire:submit.prevent="save">
+        <div class="form-inputs">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" wire:model="name">
+                @error('name')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" wire:model="email">
+                @error('email')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <button type="submit">Add Contact</button>
+    </form>
+
+    @if (count($contacts))
+        <table class="contacts">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+            @foreach ($contacts as $contact)
+                <tr>
+                    <td>{{ $contact['name'] }}</td>
+                    <td>{{ $contact['email'] }}</td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
 </div>
