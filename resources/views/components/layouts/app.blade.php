e1dcae141af8b5c046c2a75c7a7178aa04bcc284
Gagandeep Singh <gagansday@gmail.com>
2024-06-02 04:21:16 +0530

    init livewire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>Welcome to ComputeSphere</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Welcome to ComputeSphere' }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <link href="/assets/css/styles.css" rel="stylesheet">
-    <script src="https://unpkg.com/htmx.org@1.4.0"></script>
 </head>
 
 <body>
@@ -21,21 +23,7 @@
         <div class="container flex-center">
             <p class="logo-text">Welcome to <span class="logo-gradient">ComputeSphere</span></p>
 
-            <div class="quote-container">
-                <h3>
-                    Contact Manager Example
-                </h3>
-                <p class="description text">This tool allows you to manage contacts using the form below.</p>
-                {{-- <div class="quote" id="quote-text" hx-get="/quote" hx-swap="innerHTML">
-
-                </div>
-
-                <div class="flex-center mt-8">
-                    <button class="new-quote" hx-get="/quote" hx-target="#quote-text" hx-swap="innerHTML">
-                        Generate Quote
-                    </button>
-                </div> --}}
-            </div>
+            {{ $slot }}
             <br>
             <a class="text link" href="https://docs.computesphere.com/">Explore our documentation</a>
             <a class="text link" href="https://support.computesphere.com/">Reach out to support</a>
---
+++ resources/views/livewire/contact.blade.php
@@ -0,0 +1,7 @@
+<div class="contact-container">
+    <h3>
+        Contact Manager Example
+    </h3>
+    <p class="description text">This tool allows you to manage contacts using the form below.</p>
+
+</div>
